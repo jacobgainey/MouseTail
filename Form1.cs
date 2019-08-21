f2947d5fe8a606c0f650ad66bf75853fc200b49d
Jacob Noonan Gainey <jgainey@carolina.rr.com>
2019-08-21 19:22:03 -0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MouseTail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Watcher.FileWatcher fileWatcher1 = new Watcher.FileWatcher(@"D:\test\test1.txt");
+            fileWatcher1.OnChanged += FileWatcher_OnChanged;
+            fileWatcher1.OnCreated += FileWatcher_OnCreated;
+            fileWatcher1.OnDeleted += FileWatcher_OnDeleted;
+            fileWatcher1.OnRenamed += FileWatcher_OnRenamed;
+            Watcher.FileWatcher fileWatcher2 = new Watcher.FileWatcher(@"D:\test\test2.txt");
+            fileWatcher2.OnChanged += FileWatcher_OnChanged;
+            fileWatcher2.OnCreated += FileWatcher_OnCreated;
+            fileWatcher2.OnDeleted += FileWatcher_OnDeleted;
+            fileWatcher2.OnRenamed += FileWatcher_OnRenamed;
+        }
+
+        public void UpdateListBox(string message)
+        {
+            if (!InvokeRequired)
+            {
+                listBox1.Items.Add($"{message}");
+            }
+            else
+            {
+                Invoke(new Action<string>(UpdateListBox), message);
+            }
+        }
+
+        private void FileWatcher_OnChanged(object sender, FileSystemEventArgs e)
+        {
+            UpdateListBox($"[{DateTime.Now}] CHANGED - {e.Name}");
+        }
+
+        private static void FileWatcher_OnCreated(object sender, FileSystemEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"[{DateTime.Now}] CREATED - {e.Name}");
+        }
+
+        private static void FileWatcher_OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[{DateTime.Now}] DELETED - {e.Name}");
+        }
+
+        private static void FileWatcher_OnRenamed(object sender, FileSystemEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"[{DateTime.Now}] RENAMED - {e.Name}");
+        }
+    }
+}
---
+++ Watchers/FileWatcher.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+
+namespace MouseTail.Watcher
+{
+    public class FileWatcher 
+    {
+        public event FileSystemEventHandler OnChanged;
+
+        public event FileSystemEventHandler OnCreated;
+
+        public event FileSystemEventHandler OnDeleted;
+
+        public event FileSystemEventHandler OnRenamed;
+
+        public FileWatcher(string fileName)
+        {
+            FileInfo fileInfo = new FileInfo(fileName);
+
+            // instantiate the object
+            FileSystemWatcher fileSystemWatcher = new FileSystemWatcher();
+
+            // event handlers with the events
+            fileSystemWatcher.Changed += Changed;
+            fileSystemWatcher.Created += Created;
+            fileSystemWatcher.Deleted += Deleted;
+            fileSystemWatcher.Renamed += Renamed;
+
+            // tell the watcher where to look
+            fileSystemWatcher.Path = fileInfo.DirectoryName;
+            fileSystemWatcher.Filter = fileInfo.Name;
+
+            // allow events to fire.
+            fileSystemWatcher.EnableRaisingEvents = true;
+        }
+
+        private void Changed(object sender, FileSystemEventArgs e)
+        {
+            FileSystemEventHandler handler = OnChanged;
+            handler?.Invoke(this, e);
+        }
+
+        public void Created(object sender, FileSystemEventArgs e)
+        {
+            FileSystemEventHandler handler = OnCreated;
+            handler?.Invoke(this, e);
+        }
+
+        public void Deleted(object sender, FileSystemEventArgs e)
+        {
+            FileSystemEventHandler handler = OnDeleted;
+            handler?.Invoke(this, e);
+        }
+
+        public void Renamed(object sender, RenamedEventArgs e)
+        {
+            FileSystemEventHandler handler = OnRenamed;
+            handler?.Invoke(this, e);
+        }
+    }
+}
