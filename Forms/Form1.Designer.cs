0a7928d32b0357a8f4d9af763868566cd57f9145
Jacob Noonan Gainey <jgainey@carolina.rr.com>
2019-08-21 20:42:55 -0400

    added ui tabs for watching files

--- Forms/Form1.Designer.cs
+++ Forms/Form1.Designer.cs
@@ -28,45 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // listBox1
+            // openFileDialog1
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(10, 12);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(469, 121);
-            this.listBox1.TabIndex = 0;
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // button1
+            // tabControl1
             // 
-            this.button1.Location = new System.Drawing.Point(404, 139);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(498, 233);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 257);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(498, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(498, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(491, 168);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.listBox1);
+            this.ClientSize = new System.Drawing.Size(498, 279);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
     }
 }
 
--- Forms/Form1.cs
+++ Forms/Form1.cs
@@ -7,39 +7,28 @@ namespace MouseTail
 {
     public partial class Form1 : Form
     {
-        private List<Watcher.FileWatcher> FileWatcherList;
+        private List<Watcher.FileWatcher> FileWatcherList = new List<Watcher.FileWatcher>();
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            Watcher.FileWatcher fileWatcher = new Watcher.FileWatcher(@"D:\test\test1.txt");
-            fileWatcher.OnChanged += FileWatcher_OnChanged;
-            fileWatcher.OnCreated += FileWatcher_OnCreated;
-            fileWatcher.OnDeleted += FileWatcher_OnDeleted;
-            fileWatcher.OnRenamed += FileWatcher_OnRenamed;
-
-            FileWatcherList.Add(fileWatcher);
-        }
-
-        public void UpdateListBox(string message)
-        {
-            if (!InvokeRequired)
-            {
-                listBox1.Items.Add($"{message}");
-            }
-            else
-            {
-                Invoke(new Action<string>(UpdateListBox), message);
-            }
-        }
+        //public void UpdateListBox(string message)
+        //{
+        //    if (!InvokeRequired)
+        //    {
+        //        listBox1.Items.Add($"{message}");
+        //    }
+        //    else
+        //    {
+        //        Invoke(new Action<string>(UpdateListBox), message);
+        //    }
+        //}
 
         private void FileWatcher_OnChanged(object sender, FileSystemEventArgs e)
         {
-            UpdateListBox($"[{DateTime.Now}] CHANGED - {e.Name}");
+            //UpdateListBox($"[{DateTime.Now}] CHANGED - {e.Name}");
         }
 
         private static void FileWatcher_OnCreated(object sender, FileSystemEventArgs e)
@@ -58,6 +47,35 @@ namespace MouseTail
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine($"[{DateTime.Now}] RENAMED - {e.Name}");
+        }
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //OpenFile();
+            TabPage tabPage = new TabPage("New");
+            ListBox listBox = new ListBox();
+
+            listBox.Items.Add($"Hello {DateTime.Now}");
+            listBox.Dock = DockStyle.Fill;
+            listBox.IntegralHeight = false;
+
+            tabPage.Controls.Add(listBox);
+            tabControl1.TabPages.Add(tabPage);
+
+        }
+
+        private void OpenFile()
+        {
+            openFileDialog1.InitialDirectory = @"D:\test";
+            openFileDialog1.ShowDialog();
+
+            Watcher.FileWatcher fileWatcher = new Watcher.FileWatcher(openFileDialog1.FileName);
+            fileWatcher.OnChanged += FileWatcher_OnChanged;
+            fileWatcher.OnCreated += FileWatcher_OnCreated;
+            fileWatcher.OnDeleted += FileWatcher_OnDeleted;
+            fileWatcher.OnRenamed += FileWatcher_OnRenamed;
+
+            FileWatcherList.Add(fileWatcher);
         }
     }
 }
